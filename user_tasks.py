05dfa2166a08c56858e6915b63f445fbab283967
barrzik <dannybarsik@ya.ru>
2021-01-19 20:33:42 +0300

    make some benefits

--- user_tasks.py
+++ user_tasks.py
@@ -1,59 +1,44 @@
 import os
 import requests
 import datetime
-
+from pathlib import Path
 
 # создание директории tasks
 if not os.path.exists('./tasks'):
     os.makedirs('./tasks')
 
 
-# получение списка задач
-todos_response = requests.get('https://json.medrating.org/todos')
-todos_response = todos_response.json()
-
-
-# получение списка пользователей
-users_response = requests.get('https://json.medrating.org/users')
-users_response = users_response.json()
-
-
-# получение актуального времени составления отчета
-now = datetime.datetime.now()
+def get_json():
+    response = {}
+    todos_response = requests.get('https://json.medrating.org/todos')
+    todos_response = todos_response.json()
+    response['todos'] = todos_response
+    users_response = requests.get('https://json.medrating.org/users')
+    users_response = users_response.json()
+    response['users'] = users_response
+    return response
 
 
 # создание новых отчетов и перезапись старых
 def create_file(report, name):
-    # создание отчетов со старыми данными и перезапись актуальных отчетов
     if os.path.isfile(f'./tasks/{name}.txt'):
         # получение даты старого отчета
         f_old = open(f'./tasks/{name}.txt', 'r')
         old = f_old.readlines()
         f_old.close()
+        # берем из второй строки файла дату и время (два последних слова)
         old_date = old[1].split()
         old_date = old_date[-2:]
+        # форматируем дату и время по заданию
         old_date = 'T'.join(old_date)
 
-        # получение текста старого отчета
-        f_old = open(f'./tasks/{name}.txt', 'r')
-        old_text = f_old.read()
-        f_old.close()
+        p = Path(f'./tasks/{name}.txt')
+        p.rename(f'./tasks/old_{name}_{old_date}.txt')
 
-        # запись актуального отчета в актуальный файл
-        f = open(f'./tasks/{name}.txt', 'w')
-        f.write(report)
-        f.close()
-
-        # запись старого отчета в файл с требуемым названием
-        f_new_old = open(f'./tasks/old_{ name }_{ old_date }.txt', 'w')
-        f_new_old.write(old_text)
-        f_new_old.close()
-
-    # создание актуального отчета, если еще не существует
-    else:
-        f = open(f'./tasks/{name}.txt', 'w')
-        f.write(report)
-        f.close()
+    # запись актуальных данных
+    f = open(f'./tasks/{name}.txt', 'w')
+    f.write(report)
+    f.close()
 
 
 # формирование строк заголовков задач в соответствии с заданной длиной
@@ -68,37 +53,58 @@ def task_titles(title):
 
 # получение количества задач (общих/завершенных/незавершенных) и формирование списков заголовков задач
 def get_tasks_status_and_titles(user_id, todos):
-    all_tasks = 0
-    completed_tasks = 0
-    uncompleted_tasks = 0
-    completed_titles = ''
-    uncompleted_titles = ''
+    tasks = {
+        'all_tasks': 0,
+        'completed_tasks': 0,
+        'uncompleted_tasks': 0,
+        'completed_titles': '',
+        'uncompleted_titles': ''
+    }
+
     for task in todos:
         if task.get('userId') == user_id:
-            all_tasks += 1
+            tasks['all_tasks'] += 1
             if task['completed']:
-                completed_tasks += 1
-                completed_titles += task_titles(task['title'])
+                tasks['completed_tasks'] += 1
+                tasks['completed_titles'] += task_titles(task['title'])
             else:
-                uncompleted_tasks += 1
-                uncompleted_titles += task_titles(task['title'])
-    return all_tasks, completed_tasks, uncompleted_tasks, completed_titles, uncompleted_titles
+                tasks['uncompleted_tasks'] += 1
+                tasks['uncompleted_titles'] += task_titles(task['title'])
+    return tasks
 
 
 # составление отчета по пользователю и вызов функции создания файлов с отчетами
-def make_report(current_user, todos):
-    if current_user.get('name'):
-        task_status = get_tasks_status_and_titles(current_user['id'], todos)
-        report = f'Отчёт для { current_user.get("company")["name"] }. \n'
-        report += f'{ current_user["name"]} <{ current_user["email"]}> ' + now.strftime('%d.%m.%Y %H:%M') + '\n'
-        report += f'Всего задач: { task_status[0] } \n \n'
-        report += f'Завершённые задачи: ({ task_status[1] }) \n'
-        report += task_status[3] + '\n'
-        report += f'Оставшиеся задачи: ({ task_status[2] }) \n'
-        report += task_status[4] + '\n'
-        create_file(report, current_user['name'])
+def make_report(user_entry, todos, report_time):
+    if user_entry.get('name'):
+        task_status = get_tasks_status_and_titles(user_entry['id'], todos)
+        report = f'''Отчёт для {user_entry.get("company")["name"]}.
+{user_entry["name"]} <{user_entry["email"]}> {report_time.strftime('%d.%m.%Y %H:%M')}
+Всего задач: {task_status['all_tasks']}
+
+Завершённые задачи: ({task_status['completed_tasks']}) 
+{task_status['completed_titles']}
+Оставшиеся задачи: ({task_status['uncompleted_tasks']})
+{task_status['uncompleted_titles']}
+'''
+        create_file(report, user_entry['name'])
 
 
 # вызов функции составления отчета для каждого пользователя
-for user in users_response:
-    make_report(user, todos_response)
+def main():
+    now = datetime.datetime.now()
+    try:
+        response = get_json()
+    except requests.exceptions.ConnectionError as e:
+        print('Ошибка получения данных с сервера')
+        raise e
+
+    for user in response['users']:
+        try:
+            make_report(user_entry=user, todos=response['todos'], report_time=now)
+        except Exception as e:
+            print(f'Ошибка при обработке пользователя { user["id"] }')
+            print(e)
+
+
+if __name__ == '__main__':
+    main()
